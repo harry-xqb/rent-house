cf0c88050b0fdc5ad96f8e57b866b7d3b57e9828
harry-xqb <923243595@qq.com>
2020-05-08 18:17:00 +0800

    测试ssh

--- src/main/java/com/harry/renthouse/RentHouseApplication.java
+++ src/main/java/com/harry/renthouse/RentHouseApplication.java
@@ -3,7 +3,7 @@ package com.harry.renthouse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication 
 public class RentHouseApplication {
 
     public static void main(String[] args) {
